9ee660711ecb572798ba4ca9d239c9fcd720792e
Muzamil <itmozamil22@outlook.com>
2021-10-23 21:23:04 +0200

    add toast message to phone field

--- app/src/main/java/com/kldmohammed/gittutorial/MainActivity.kt
+++ app/src/main/java/com/kldmohammed/gittutorial/MainActivity.kt
@@ -4,18 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         findViewById<Button>(R.id.loginBtn).setOnClickListener {
           val phone= findViewById<EditText>(R.id.phone).text.toString()
 
-           if (Validator.validatePhone(phone)){
-
-
+           if (!Validator.validatePhone(phone)){
+               Toast.makeText(this,"the phone is not valid",Toast.LENGTH_LONG).show()
            }
 
         }
